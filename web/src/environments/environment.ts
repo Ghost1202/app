423a01968452d07aaf575600f17203b0aabe6b1d
Ghost1202 <ehor760@gmail.com>
2025-12-11 11:52:45 +0000

    edit environments1

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://api:8080/api/v1/todo'
+  production: false, // или true для продакшена
+  apiUrl: 'http://3.124.219.135:8080' // внешний IP или домен сервера
 };
