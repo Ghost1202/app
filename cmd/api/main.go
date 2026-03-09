065dd8f375d592b0b1a9ac543f481401cf92d1df
Ghost1202 <ehor760@gmail.com>
2026-03-09 17:09:40 +0000

    add new clear code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	_ "github.com/mecitsemerci/go-todo-app/docs"
-	"github.com/mecitsemerci/go-todo-app/internal/api"
-	"log"
+
 )
 
-// @title Todo API
-// @version 1.0.0
-// @description This is a sample todo restful api server.
-// @host localhost:8080
-// @BasePath /
 func main() {
 	app, err := api.NewAppServer()
 
